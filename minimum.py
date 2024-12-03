e0630ebe0a445cd3976a63603d918cc00c040847
Ayyappa Gandikota <coder.honey.915@gmail.com>
2024-12-03 23:41:39 +0530

    Add files via upload

---
+++ minimum.py
@@ -0,0 +1,32 @@
+def enquiry():
+    res=input("Are u want to continue the calculation:(yes/no) [case sensitive] ?")
+    s1="yes"
+    if res == s1: 
+        min()
+    else:
+        print(".....THANK YOU .....")
+        print("........BYE.........")
+        exit()
+def main():
+    print()
+    print("....Welcome to maximum & minimum number identifier....")
+def min():
+    a=int(input("Enter value for a:"))
+    b=int(input("Enter value for b:"))
+    c=int(input("Enter value for c:"))
+    if (a<b) and (a<c):
+        print("{0} is minimum number among {1},{2},{3} numbers".format(a,a,b,c))
+        print()
+        enquiry()
+    elif (b<a) and (<>c):
+        print("{0} is minimum number among {1},{2},{3} numbers".format(b,a,b,c))
+        print()
+        enquiry()
+    else:
+        print("{0} is minimum number among {1},{2},{3} numbers".format(c,a,b,c))
+        print()
+        enquiry()
+main()
+max()
+
+
